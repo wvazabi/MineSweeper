46a4ded615df1174faf614469c7bfc38dc7bf6d1
Enes Kaya <eng.eneskaya@gmail.com>
2024-03-06 00:55:28 +0300

    Add Mİne Count Finder

--- MineSweeper/src/Main.java
+++ MineSweeper/src/Main.java
@@ -19,44 +19,134 @@ public class Main {
     }
 
 
-    public static boolean isMineCheck() {
-        return false;
+    public static int mineNumber(int element) {
+
+        return element / 4;
+    }
+
+    public static int neighborMineCount(String[][] arr, int row, int col) {
+        int count = 0;
+        for (int i = row - 1; i <= row + 1; i++) {
+            for (int j = col - 1; j <= col + 1; j++) {
+                if ((i >= 0 && i < arr.length) && (j >= 0 && j < arr[0].length)) {
+                    if (arr[i][j].equals("*")) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
     }
 
 
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
 
         int row = 0;
-        int column = 0;
+        int col = 0;
 
         do {
             System.out.println("Enter row: ");
             isInt(input);
             row = input.nextInt();
-            System.out.println("Enter column: ");
+            System.out.println("Enter col: ");
             isInt(input);
-            column = input.nextInt();
+            col = input.nextInt();
 
-        } while (!isCheck(row, column));
+        } while (!isCheck(row, col));
 
-        String[][] arr = new String[row][column];
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
+        String[][] arr = new String[row][col];
+        String[][] mineArray = new String[row][col];
 
-                arr[i][j] = "_";
+        int elementNum = (row * col);
 
+        // Mine Count
+        int mineNumber = mineNumber(elementNum);
 
-            }
+        int 
+
+        System.out.println("mine num = " + mineNumber);
+
+        int mineRow, mineCol;
+
+        for (int i = 0; i < mineNumber; i++) {
+            mineRow = (int) (Math.random() * row);
+            mineCol = (int) (Math.random() * col);
+            mineArray[mineRow][mineCol] = "*";
         }
 
+        System.out.println("\nMines Locations");
+
         for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                System.out.print(arr[i][j] + " ");
+            for (int j = 0; j < col; j++) {
+                if (mineArray[i][j] == null) {
+                    mineArray[i][j] = "-";
+                }
+                System.out.print(mineArray[i][j] + " ");
             }
             System.out.println();
 
         }
+
+        // User input
+        int userRow, userCol;
+
+        do {
+            int temp = 0;
+
+            System.out.print("Enter destination row : ");
+            isInt(input);
+            userRow = input.nextInt();
+
+            System.out.print("Enter destination col : ");
+            isInt(input);
+            userCol = input.nextInt();
+
+            if (!((userRow < row && userRow >= 0) && (userCol >= 0 && userCol < col))) {
+                System.out.println("Please try again!!");
+            }
+
+            temp = neighborMineCount(mineArray,userRow,userCol);
+           // System.out.println(temp);
+
+            String mineCount = String.valueOf(temp);
+
+            if(mineArray[userRow][userCol].equals("*")) {
+                System.out.println("GAME OVER !!!");
+                break;
+            }
+
+            mineArray[userRow][userCol] = mineCount;
+
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    System.out.print(mineArray[i][j] + " ");
+                }
+                System.out.println();
+
+            }
+
+
+        } while (!mineArray[userRow][userCol].equals("*"));
+
+
+//        for (int i = 0; i < row; i++) {
+//            for (int j = 0; j < col; j++) {
+//
+//                arr[i][j] = "_";
+//
+//
+//            }
+//        }
+//
+//        for (int i = 0; i < row; i++) {
+//            for (int j = 0; j < col; j++) {
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//
+//        }
 
 
     }
